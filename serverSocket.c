c2dbbdcb7db8802e9b34aecc18ef4e5e1305c0ad
GitMeACoffeeToday <129101783+GitMeACoffeeToday@users.noreply.github.com>
2024-03-16 18:43:22 -0700

    Commit 1
    Added initial functionality for server side.

--- serverSocket.c
+++ serverSocket.c
@@ -5,5 +5,32 @@
 #include <netinet/in.h>
 
 int main(){
-  
+	char server_message[300] = "You have reached the server.\n";
+
+	// creates the server socket
+	int server_socket;
+	server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	// define server address
+	struct sockaddr_in server_address;
+
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(9002);
+	server_address.sin_addr = INADDR_ANY;
+
+	// bind the socket to an address
+	bind(server_socket, (struct sockaddr) &server_address, sizeof(server_address));
+
+	// Listen for incoming connections, set backlog to 5 for now.
+	listen(server_socket, 5);
+
+	// client socket
+	int client_socket;
+	client_socket = accept(server_socket, NULL, NULL); // Other two fields left as NULL for now.
+
+	// send message
+	send(client_socket, server_message, sizeof(server_message));
+
+	// close the socket when done.
+	close(server_socket);
 }
