c8567f0d0029bc9d60616d1ec092acbfe5917719
GitMeACoffeeToday <129101783+GitMeACoffeeToday@users.noreply.github.com>
2024-03-16 18:23:33 -0700

    Commit 1
    Added initial code for Client socket side.

--- clientSocket.c
+++ clientSocket.c
@@ -4,3 +4,37 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+int main(){
+
+	// creating a socket...
+
+	int network_socket;
+	network_socket = socket(AF_INET, SOCK_STREAM, 0); // Network socket created
+
+	// give the socket an address
+
+	struct sockaddr_in server_address;
+
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(9002);
+	server_address.sin_addr.s_addr = INADDR_ANY;
+
+	int connect_success = connect(network_socket, (struct sockaddr) &server_address, sizeof(server_address));
+
+	char server_response[300];
+
+	if(connect_success == -1){
+		printf("Client failed to connect to the Server!\n");
+	}
+
+	else if(connect_success == 0){
+		// Client recieves data from Server which is stored in server_response
+		recv(network_socket, &server_response, sizeof(server_response), 0);
+		printf("Server Data: %s\n", server_response);
+	}
+	
+	// Close the socket when done...
+	close(sock);
+
+	return 0;
+}
