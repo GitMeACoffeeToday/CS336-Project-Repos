cf95197dcf9b0f268a55639355a200485476adc3
GitMeACoffeeToday <129101783+GitMeACoffeeToday@users.noreply.github.com>
2024-03-20 19:13:29 -0700

    Update clientSocket.c

--- clientSocket.c
+++ clientSocket.c
@@ -5,8 +5,10 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <string.h>
 
-#include "/home/michael/Documents/CS336 CLIENT C Files/cJSON.h"
+#include "cJSON.h"
+#include "cJSON.c"
 
 struct configs{ // Default value set.
 	char serverIPAddr[100];
@@ -147,40 +149,40 @@ int main(int argc, char *argv[]){
 
 			printf("IP ADDRESS: %s\n", configuration->serverIPAddr);
 			printf("Source Port Number: %d\n", configuration->sourcePortNum);
-		}
-		close(configfile); // close file afterwards.
-	}
 
-	// creating a socket...
-	int network_socket;
-	network_socket = socket(AF_INET, SOCK_STREAM, 0); // Network socket created
+			// creating a socket...
+			int network_socket;
+			network_socket = socket(AF_INET, SOCK_STREAM, 0); // Network socket created
 
-	// give the socket an address
+			// give the socket an address
 
-	struct sockaddr_in server_address;
+			struct sockaddr_in server_address;
 
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(9002);
-	inet_pton(AF_INET, "192.168.128.2", &(server_address.sin_addr)); // Change IP to that of the SERVER.
+			server_address.sin_family = AF_INET;
+			server_address.sin_port = htons(9002);
+			inet_pton(AF_INET, "192.168.128.2", &(server_address.sin_addr)); // Change IP to that of the SERVER.
 
-	int connect_success = connect(network_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+			int connect_success = connect(network_socket, (struct sockaddr*) &server_address, sizeof(server_address));
 
-	char server_response[300];
+			char server_response[300];
 
-	if(connect_success == -1){
-		printf("Client failed to connect to the Server!\n");
-	}
+			if(connect_success == -1){
+				printf("Client failed to connect to the Server!\n");
+			}
 
-	else if(connect_success == 0){
-		printf("...Connect Success!\n");
-		
-		// Client recieves data from Server which is stored in server_response
-		recv(network_socket, &server_response, sizeof(server_response), 0);
-		printf("Server Data: %s\n", server_response);
-	}
+			else if(connect_success == 0){
+				printf("...Connect Success!\n");
+				
+				// Client recieves data from Server which is stored in server_response
+				recv(network_socket, &server_response, sizeof(server_response), 0);
+				printf("Server Data: %s\n", server_response);
+			}
 	
-	// Close the socket when done...
-	close(network_socket);
+			// Close the socket when done...
+			close(network_socket);
 
+		}
+		fclose(configfile); // close file afterwards.
+	}
 	return 0;
 }
