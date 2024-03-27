c6eda518693edc833157445b183d6b06d846534f
GitMeACoffeeToday <129101783+GitMeACoffeeToday@users.noreply.github.com>
2024-03-26 19:46:13 -0700

    Update serverSocket.c

--- serverSocket.c
+++ serverSocket.c
@@ -209,7 +209,7 @@ void serverProbingPhase(struct configs* serverConfig){
 		}
 	}
 
-	printf("Final Calculation: %ld\n", (high_entropy_seconds2 - high_entropy_seconds1) - (low_entropy_seconds2 - low_entropy_seconds1));
+	printf("Final Calculation: %d\n", abs((high_entropy_seconds2 - high_entropy_seconds1) - (low_entropy_seconds2 - low_entropy_seconds1));
 
 	//recvfrom(server_socket, client_message, 100, 0, (struct sockaddr*) &server_address, NULL);
 	//printf("Client Response: %s\n", client_message);
