21519f501ca1a66807a4393830708f7191b31ff9
GitMeACoffeeToday <129101783+GitMeACoffeeToday@users.noreply.github.com>
2024-03-26 20:00:02 -0700

    Update clientSocket.c

--- clientSocket.c
+++ clientSocket.c
@@ -215,6 +215,37 @@ void clientProbingPhase(struct configs* clientConfig){
 	close(network_socket);
 }
 
+void recvFromServerTCP(){
+	// creating a socket...
+			int network_socket;
+			network_socket = socket(AF_INET, SOCK_STREAM, 0); // Network socket created
+
+			// give the socket an address
+			struct sockaddr_in server_address;
+
+			server_address.sin_family = AF_INET;
+			server_address.sin_port = htons(9002);
+			inet_pton(AF_INET, "192.168.128.2", &(server_address.sin_addr)); // Change IP to that of the SERVER.
+
+			int connect_success = connect(network_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+			char server_response[2];
+
+			if(connect_success == -1){
+				printf("Client failed to connect to the Server!\n");
+			}
+
+			else if(connect_success == 0){
+				printf("...Connection Success!\n");
+				
+				// Client recieves data from Server which is stored in server_response
+				recv(network_socket, &server_response, sizeof(server_response), 0);
+				printf("Server Response: %s\n", server_response);
+			}
+	
+			// Close the socket when done...
+			close(network_socket);
+}
+
 
 int main(int argc, char *argv[]){
 	struct configs clientConfig;
@@ -239,6 +270,8 @@ int main(int argc, char *argv[]){
 		clientToServerConfig(argv[1]);
 		sleep(5);
 		clientProbingPhase(&clientConfig);
+		sleep(5);
+		recvFromServerTCP();
 	}
 	return 0;
 }
